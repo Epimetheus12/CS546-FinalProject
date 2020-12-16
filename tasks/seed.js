171fe875f4162a5660cc70ab321053beaffbf64d
JoeyIsZY <1933920443@qq.com>
2020-12-16 01:55:21 -0500

    Add files via upload

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,5 +1,27 @@
-const { usersData } = require("../data");
-
-
 const data = require("../data");
+const userTemp = require("./user");
+const bcrypt = require("bcrypt");
 
+
+async function addUser() {
+    for (let i = 0; i < userTemp.length; i++) {
+        try {
+            let pwd = await bcrypt.hash(userTemp[i].password, 12);
+            await data.usersData.create(
+                userTemp[i].username,
+                userTemp[i].email,
+                pwd,
+                userTemp[i].gender,
+                userTemp[i].nickname);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+async function main() {
+    await addUser();
+}
+
+
+main();
---
+++ tasks/user.js
@@ -0,0 +1,72 @@
+module.exports = [
+    {
+        username: "HarryPotter",
+        email: "19339204443@qq.com",
+        password: "Aa123456!",
+        gender: "male",
+        nickname: "harry0603"
+    },
+    {
+        username: "DanpingCai",
+        email: "5478131414@qq.com",
+        password: "Bb235789$",
+        gender: "female",
+        nickname: "caicai1229"
+    },
+    {
+        username: "FangjiLiang",
+        email: "zy9663@outlook.com",
+        password: "Cc245663!",
+        gender: "male",
+        nickname: "Liang1225"
+    },
+    {
+        username: "ZhiliYu",
+        email: "fjl1225@outlook.com",
+        password: "Dd33425645#",
+        gender: "male",
+        nickname: "Yuzhili001"
+    },
+    {
+        username: "Yuzhou",
+        email: "yzhou100@outlook.com",
+        password: "Ee123384%",
+        gender: "male",
+        nickname: "yuzhou001"
+    },
+    {
+        username: "Aotianlong",
+        email: "aotianlong100@outlook.com",
+        password: "Ironman342!",
+        gender: "male",
+        nickname: "user002"
+    },
+    {
+        username: "Emma",
+        email: "emma100@outlook.com",
+        password: "Wason122323!",
+        gender: "female",
+        nickname: "user003"
+    },
+    {
+        username: "James",
+        email: "james100@outlook.com",
+        password: "James666666!",
+        gender: "male",
+        nickname: "user004"
+    },
+    {
+        username: "ReBron",
+        email: "rebron100@outlook.com",
+        password: "reBron666!",
+        gender: "male",
+        nickname: "user005"
+    },
+    {
+        username: "Anthony",
+        email: "anny100@outlook.com",
+        password: "Answer1234!",
+        gender: "male",
+        nickname: "user006"
+    }
+];
