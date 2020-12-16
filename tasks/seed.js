e3dc7ebecf166440631c2b3ca84a4c02f43b5861
JoeyIsZY <1933920443@qq.com>
2020-12-16 03:00:38 -0500

    Add files via upload

--- tasks/seed.js
+++ tasks/seed.js
@@ -2,17 +2,25 @@ const data = require("../data");
 const userTemp = require("./user");
 const bcrypt = require("bcrypt");
 
+const commentList = [{detail: "Good.", level: 1}, {detail: "Not bad.", level: 1},
+    {detail: "Awesome.", level: 1}, {detail: "Pretty good.", level: 1},
+    {detail: "I don't like it.", level: 1}, {detail: "I like it.", level: 1},
+    {detail: "It is a shit.", level: 1}, {detail: "I don't like this actor..", level: 1},
+    {detail: "This actress is really sexy.", level: 1}, {detail: "Nintendo is the master of the world.", level: 1}];
+
 
 async function addUser() {
     for (let i = 0; i < userTemp.length; i++) {
         try {
             let pwd = await bcrypt.hash(userTemp[i].password, 12);
-            await data.usersData.create(
+            let user = await data.usersData.create(
                 userTemp[i].username,
                 userTemp[i].email,
                 pwd,
                 userTemp[i].gender,
                 userTemp[i].nickname);
+
+            await data.commentsData.create(user._id, commentList[i].detail, commentList[i].level);
         } catch (e) {
             console.log(e);
         }
