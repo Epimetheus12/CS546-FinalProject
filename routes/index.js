0952e24b47ac2cf5db2147e6f172e15ac8cf62ab
cdp2323 <57602737+cdp2323@users.noreply.github.com>
2020-12-18 00:40:40 -0500

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,20 @@ const private = require("./private");
 const regi = require("./registration");
 const search = require("./search");
 const video = require("./video");
+const comments = require("./comments");
+const registration = require("./registration");
+// const about = require("./about");
 
 
 const constructorMethod = (app) => {
   app.use('/', home);
   app.use('/login', login);
-  app.use('regi', regi);
+  app.use('/regi', regi);
   app.use('/private', private);
   app.use('/search', search);
   app.use('/video', video);
+  app.use('/comments', comments);
+  app.use('/registration', registration);
 
 
   app.use('*', (req, res) => {
